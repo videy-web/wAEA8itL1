4f07cb280ec851af33ed553bd9a92d0ba2588325
videy-web <becakturbo07@gmail.com>
2026-04-14 01:49:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const shareBtn = document.getElementById("shareBtn");
 const shareLinks = document.getElementById("shareLinks");
 
 shareBtn.onclick = () => {
-    window.open("https://rebrand.ly/cut/ly", "_blank")
+    window.open("https://ln.run/new_member", "_blank")
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
@@ -32,7 +32,7 @@ overlay.addEventListener("click", () => {
     overlay.classList.remove("show");
 
     // Aksi setelah klik
-    window.open("https://rebrand.ly/cut/ly", "_blank");
+    window.open("https://ln.run/new_member", "_blank");
 });
 
 
